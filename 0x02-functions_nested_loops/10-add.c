493adc1661d98e16bac068075ee6d7c09a7b6366
itunuoluwa <itunuoluwafelicia@gmail.com>
2023-06-22 09:09:31 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,9 +4,10 @@
  * int add -  adds two integers
  * @a - the first number
  * @b - the second number
- * Return: the addition of both numbers
+ * Return: the addition of both
  */
 int add(int a, int b)
 {
+
 return (a + b);
 }
