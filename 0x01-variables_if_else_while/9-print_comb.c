5cccf937edc6d0690a227876c1000eaf51afd85c
itunuoluwa <itunuoluwafelicia@gmail.com>
2023-06-25 22:10:09 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: prints all possible combinations of single-digit numbers
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar (n);
+		putchar (',');
+		putchar (' ');
+		n = n + 1;
+	}
+		putchar ('\n');
+		return (0);
+}
