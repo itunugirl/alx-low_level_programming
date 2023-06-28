101a666d65911999fba34928d5b9c57b4cfb0235
itunuoluwa <itunuoluwafelicia@gmail.com>
2023-06-27 20:33:46 -0700

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,11 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i])
 		i++;
-	for (i = i - 1; i >= 0; i--)
-		_putchar (s[i]);
-
-	putchar('\n');
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
