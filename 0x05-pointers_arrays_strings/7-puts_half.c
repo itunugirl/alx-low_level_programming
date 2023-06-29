af1319fb097802a1626d87326eb7cedfe2d821b0
itunuoluwa <itunuoluwafelicia@gmail.com>
2023-06-29 10:31:25 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - prints half of a string
+ * @str: prints half of a string
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	unsigned int i;
+
+	for (i = strlen(str) / 2; i < strlen(str); i++)
+	{
+		printf("%c", str[i]);
+	}
+	printf("\n");
+}
