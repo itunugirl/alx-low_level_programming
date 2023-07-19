d94a2a41b9df588bb71b321326485a927a1c023a
itunugirl <itunuoluwafelicia@gmail.com>
2023-07-19 09:36:17 -0700

    0x0E-preprocessor

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,11 +2,11 @@ nclude <stdlib.h>
 #include "dog.h"
 
 /**
- *  * _strlen - returns the length of a string
- *   * @s: string to evaluate
- *    *
- *     * Return: the length of the string
- *      */
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
 int _strlen(char *s)
 {
 		int i;
@@ -22,14 +22,14 @@ int _strlen(char *s)
 }
 
 /**
- *  * *_strcpy - copies the string pointed to by src
- *   * including the terminating null byte (\0)
- *    * to the buffer pointed to by dest
- *     * @dest: pointer to the buffer in which we copy the string
- *      * @src: string to be copied
- *       *
- *        * Return: the pointer to dest
- *         */
+ * _strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
 char *_strcpy(char *dest, char *src)
 {
 		int len, i;
